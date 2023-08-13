d938788c2d15261a6d2dda2e23f1aa37c95fb1e7
Sam Rizvi <srizvi@users.noreply.github.com>
2023-08-12 23:21:18 -0700

    style: Update Open graph image

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -19,15 +19,20 @@ export default async function OpengraphImage(
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-[#240934]">
-        <div tw="flex flex-none items-center justify-center border border-gray-700 h-[160px] w-[160px] rounded-3xl">
+        <div tw="flex flex-none items-center justify-center border border-gray-700 h-[160px] w-[160px] rounded-3xl shadow-sm shadow-[#09bcef]">
           <LogoIcon
-            width="64"
-            height="58"
+            width="150"
+            height="150"
             fill="#09bcef"
-            className="outline-gray-50"
+            className="p-4 outline-gray-900"
           />
         </div>
-        <p tw="mt-12 text-7xl font-bold text-[#09bcef]">{title}</p>
+        <p tw="mt-12 text-[180px] font-black uppercase tracking-wider text-gray-50 leading-none">
+          {title}
+        </p>
+        <p tw="ms-auto text-center max-w-2xl mt-4 text-lg font-normal text-gray-400">
+          Victory is a lifestyle. Excellence is a habit. Keep winning.
+        </p>
       </div>
     ),
     {
